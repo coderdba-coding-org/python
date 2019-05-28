b2ce83e11d143897a022681346fffffd07826a7c
coderdba <coderdba@coder.com>
2019-05-28 17:25:55 +0530

    first commit

--- git-init.sh
+++ git-init.sh
@@ -3,7 +3,7 @@ git config user.name coderdba
 
 echo "# python" >> README.md
 git init
-git add README.md
+git add .
 git commit -m "first commit"
 git remote add origin https://github.com/coderdba-coding-org/python.git
 git push -u origin master
